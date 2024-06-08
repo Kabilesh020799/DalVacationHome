5ca49cfe9f123fa0bbf7c724f75b95c608cfdf01
RAJ PATEL <rj268617@dal.ca>
2024-06-07 23:21:09 -0300

    feat: add support request

--- backend/lex-processor-lambda/lex-processor.py
+++ backend/lex-processor-lambda/lex-processor.py
@@ -8,6 +8,35 @@ import logging
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
+
+def raise_support_ticket(intent_request):
+    session_attributes = get_session_attributes(intent_request)
+    input_text = get_slot(intent_request, 'choice')
+    logger.info(input_text)
+
+    parts = input_text.split(':')
+    text = ''
+    response, number = parts[0].strip(), parts[1].strip()
+
+
+    if response and response.lower() == 'yes':
+        if number:
+            # send the concern to support agent
+            text = "TicketID: 1234"
+        else:
+            text = "Please login and raise a support request for your concern."
+    elif response and response.lower() == 'no':
+        text = "Thank you. Have a nice day!"
+    else:
+        text = "Invalid response. Must be 'Yes' or 'No'."
+
+    message =  {
+        'contentType': 'PlainText',
+        'content': text
+    }
+    fulfillment_state = "Fulfilled"
+    return close(intent_request, session_attributes, fulfillment_state, message)
+
 # Function to actually fetch the data.
 def fetch_data_dynamo(booking_ref_number):
     dynamodb = boto3.resource('dynamodb')
@@ -79,9 +108,12 @@ def fetch_booking_info(intent_request):
 
 def dispatch(intent_request):
     intent_name = intent_request['sessionState']['intent']['name']
+
     # Handle various intents
     if intent_name == 'BookingInformation':
         return fetch_booking_info(intent_request)
+    elif intent_name == 'SupportIntent':
+        return raise_support_ticket(intent_request)
 
     raise Exception('Intent with name ' + intent_name + ' not supported')
 
