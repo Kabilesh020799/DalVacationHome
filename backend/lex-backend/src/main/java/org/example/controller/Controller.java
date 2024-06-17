e0ab5ff9e0baccca7b10bc63476e695be24faf8c
RAJ PATEL <rj268617@dal.ca>
2024-06-17 14:42:03 -0300

    fix: resolve CORS issue

--- backend/lex-backend/src/main/java/org/example/controller/Controller.java
+++ backend/lex-backend/src/main/java/org/example/controller/Controller.java
@@ -4,6 +4,7 @@ package org.example.controller;
 import org.example.dto.ChatRequest;
 import org.example.service.LexService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -15,6 +16,7 @@ import static org.example.utils.Constants.*;
 
 @RestController
 @EnableWebMvc
+@CrossOrigin(origins = "*")
 public class Controller {
 
     private final LexService lexService;
@@ -25,8 +27,9 @@ public class Controller {
     }
 
     @PostMapping("/recognize")
-    public String recognize(@RequestBody ChatRequest chatRequest) {
+    public ResponseEntity<String> recognize(@RequestBody ChatRequest chatRequest) {
         Map<String, String> pong = new HashMap<>();
-        return lexService.welcomeRequest(BOT_ID, BOT_ALIAS_ID, LOCAL_ID, chatRequest.getSessionId(), chatRequest.getRequestText());
+        String response = lexService.welcomeRequest(BOT_ID, BOT_ALIAS_ID, LOCAL_ID, chatRequest.getSessionId(), chatRequest.getRequestText());
+        return ResponseEntity.ok(response);
     }
 }
