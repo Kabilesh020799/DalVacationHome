7a95c67e9bb66d87f1e663823e7ed94455285835
Kabilesh <kabilesh@dal.ca>
2024-07-15 20:24:26 -0300

    card component

---
+++ frontend/src/components/card/card.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+
+const Card = ({ image, name, beds }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+            component="img"
+            height="140"
+            image={image}
+            alt={name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+              {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+              Number of beds: {beds}
+          </Typography>
+      </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default Card;
