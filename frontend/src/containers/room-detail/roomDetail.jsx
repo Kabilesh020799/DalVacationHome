a8a58fc6ca850590cec60d7a6b43b367f8175978
Kabilesh <kabilesh@dal.ca>
2024-07-22 13:35:38 -0300

    room detail page

--- frontend/src/containers/room-detail/roomDetail.jsx
+++ frontend/src/containers/room-detail/roomDetail.jsx
@@ -1,19 +1,43 @@
-import React from "react";
-import { useLocation, useSearchParams } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import { rooms } from '../home/constants';
+
+const useQuery = () => {
+  return new URLSearchParams(useLocation().search);
+};
 
 const RoomDetail = () => {
-  const searchParams = useSearchParams();
-
-  const useQuery = () => {
-    return new URLSearchParams(useLocation().search);
-  };
-
   const query = useQuery();
   const roomId = query.get('roomId');
-console.log(roomId);
+  const room = rooms.find((room) => room?.id == roomId) || {};
+
   return (
     <div>
-
+      <Box
+        component="img"
+        sx={{
+          width: '100%',
+          height: '450px',
+        }}
+        alt={room.name}
+        src={room.image}
+      />
+      <Container>
+        <Box sx={{ my: 4 }}>
+          <Typography gutterBottom variant="h4" component="div">
+            {room.name}
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            Number of beds: {room.beds}
+          </Typography>
+        </Box>
+      </Container>
     </div>
   );
 };
